141a6390a07bcb42fff17c5051192ae8e48612bb
AngelBandin <73777868+AngelBandin@users.noreply.github.com>
2024-01-18 17:54:00 +0100

    Delete src directory

--- src/main/java/gal/usc/grei/cn/precios/servicio/ServicioReserva.java
+++
@@ -1,9 +0,0 @@
-package gal.usc.grei.cn.precios.servicio;
-
-import gal.usc.grei.cn.precios.modelo.Compra;
-
-public interface ServicioReserva {
-
-    boolean procesarReserva(Compra compra); //función para comprobar si la accion comprada esta disponible
-                                            //en la fecha y por la entidad indicada en la compra
-}
